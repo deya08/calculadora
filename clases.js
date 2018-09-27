85cfbd450d0b8662f04ccdaacd93657cf6500b94
Deyalik Delannoy P <deyadelanoy.08@gmail.com>
2018-09-27 15:48:16 -0500

    calculadora final

--- clases.js
+++ clases.js
@@ -1,1 +1,8 @@
-// JavaScript source code
+function  Calculadora(numA, numB){
+	this.numA=numA;
+	this.numB=numB;
+	this.sumar=function(){
+		var suma=this.numA+this.numB;
+		alert(suma);
+		return suma;	
+	}
